9efbc5da7c5cf11c8e34fbd99a7f4e7fce0fd0d5
BobCiric <jil202@pitt.edu>
2020-10-06 17:42:41 -0400

    Update disparity_ssd.m

--- ps2_matlab_template/disparity_ssd.m
+++ ps2_matlab_template/disparity_ssd.m
@@ -1,4 +1,6 @@
+
 function D = disparity_ssd(L, R)
+tic
     % Compute disparity map D(y, x) such that: L(y, x) = R(y, x + D(y, x))
     %
     % L: Grayscale left image
@@ -10,24 +12,26 @@ function D = disparity_ssd(L, R)
     win_M = floor(win/2);
     dim = size(R);
     msg = 'Creating Disparity map ...';
-    ssd(1,1) = 1;
+    ssd(1,1) = Inf;
     D = zeros(dim(1),dim(2));
     wb = waitbar(0, msg);
     for i = 2:1:dim(1)-1
         for j = 2:1:dim(2)-1
-            template = L(i-win_M:i+win_M,j-win_M:j+win_M); % template with a window size of 3
-            strip = R(i-win_M:i+win_M,1:end);
-            for jj = 2:dim(2)-1 %sliding the template across the strip
-                ssd(jj) = sum((strip(:,jj-win_M:jj+win_M) - template).^2,'all');
-                [val,loc] = min(ssd);
+            template = L(i-1:i+1,j-1:j+1); % template with a window size of 3
+            parfor jj = 2:dim(2)-1 %sliding the template across the strip
+              comp = R(i-1:i+1,jj-1:jj+1);
+              ssd(jj) = sum((comp - template).^2,'all');
             end
-            D(i,j) = j - loc;
+            [val,loc] = min(ssd);
+            D(i,j) = j-loc;
         end
+
      counter = (i/(dim(1)-win_M));
      waitbar(counter,wb)
      
     end
 delete(wb)
+toc
     end
     
     
