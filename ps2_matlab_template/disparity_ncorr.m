a5c164c9fcf183d761dd0fd550cdb38ef2c8f305
BobCiric <jil202@pitt.edu>
2020-10-07 02:10:44 -0400

    Update disparity_ncorr.m

--- ps2_matlab_template/disparity_ncorr.m
+++ ps2_matlab_template/disparity_ncorr.m
@@ -6,4 +6,26 @@ function D = disparity_ncorr(L, R)
     % D: Output disparity map, same size as L, R
 
     % TODO: Your code here
+    win = 7;
+    win_M = floor(win/2);
+    dim = size(R);
+    msg = 'Creating Disparity map ...';
+    ssd(1,1) = Inf;
+    D = zeros(dim(1),dim(2));
+    wb = waitbar(0, msg);
+    for i = 4:1:dim(1)-3
+        for j = 4:1:dim(2)-3
+            template = L(i-3:i+3,j-3:j+3); % template with a window size of 7
+              comp = R(i-3:i+3,1:end);
+              c = normxcorr2(template, comp);       
+            [ypeak,xpeak] = find(c==max(c(:)));
+            D(i,j) = j-xpeak;
+        end
+
+     counter = (i/(dim(1)-win_M));
+     waitbar(counter,wb)
+     
+    end
+delete(wb)
+toc
 end
